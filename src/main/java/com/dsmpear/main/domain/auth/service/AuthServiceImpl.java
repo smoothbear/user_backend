e9b8a0628640cd49a2cdc219694796cebbffd9be
kjbin0420 <kjbin0420@naver.com>
2020-10-23 08:36:35 +0900

    Test Commit

--- src/main/java/com/dsmpear/main/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/dsmpear/main/domain/auth/service/AuthServiceImpl.java
@@ -2,9 +2,10 @@ package com.dsmpear.main.domain.auth.service;
 
 import com.dsmpear.main.domain.auth.dto.request.SignInRequest;
 import com.dsmpear.main.domain.auth.dto.response.TokenResponse;
+import com.dsmpear.main.domain.user.domain.User;
 import com.dsmpear.main.domain.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,19 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public TokenResponse signIn(SignInRequest dto) {
-        return userRepository.findByEmail(dto.getEmail())
+        /*return userRepository.findByEmail(dto.getEmail())
                 .filter(user -> passwordEncoder.matches(dto.getPassword(), user.getPassword()))
-                    .map(Users)
+                    .map(User::getId)
+                    .map(id -> {
+                        try {
+                            authenticationManager.authenticate()
+                        }
+                    })
+                    */
+        return null;
     }
 }
--- src/main/java/com/dsmpear/main/domain/user/domain/User.java
+++ src/main/java/com/dsmpear/main/domain/user/domain/User.java
@@ -2,12 +2,15 @@ package com.dsmpear.main.domain.user.domain;
 
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
 @Getter
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
     private String password;
 }
