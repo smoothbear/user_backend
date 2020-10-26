aa7512f135024eedd229823444aa1eb31dc47ad9
kjbin0420 <kjbin0420@naver.com>
2020-10-26 20:25:36 +0900

    [PEAR-5] CI Test

--- src/main/java/com/dsmpear/main/domain/auth/controller/TokenController.java
+++ src/main/java/com/dsmpear/main/domain/auth/controller/TokenController.java
@@ -2,6 +2,8 @@ package com.dsmpear.main.domain.auth.controller;
 
 import com.dsmpear.main.domain.auth.dto.request.SignInRequest;
 import com.dsmpear.main.domain.auth.dto.response.TokenResponse;
+import com.dsmpear.main.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class TokenController {
+    private final AuthService authService;
+
     @PostMapping
     public TokenResponse signIn(@RequestBody @Valid SignInRequest dto) {
-        return null;
+        return authService.signIn(dto);
     }
 }
